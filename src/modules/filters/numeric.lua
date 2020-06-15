44ae550b56739bd2cacf4a23db6f8c012175ceb1
unrooot <64768843+unrooot@users.noreply.github.com>
2020-06-14 19:04:34 -0700

    stripped extraneous files

--- src/modules/filters/numeric.lua
+++
@@ -1,7 +0,0 @@
-return function(instance)
-	if instance:IsA("TextBox") then
-		instance:GetPropertyChangedSignal("Text"):Connect(function()
-			instance.Text = instance.Text:gsub("%D+")
-		end)
-	end
-end
