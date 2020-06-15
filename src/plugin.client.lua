06d8eb02abfc07461e8001557453902affd6eb32
unrooot <nweeek@gmail.com>
2020-06-14 19:04:34 -0700

    stripped extraneous files

--- src/plugin.client.lua
+++ src/plugin.client.lua
@@ -7,14 +7,11 @@ local toolbar = plugin:CreateToolbar("PogSuite")
 local button = toolbar:CreateButton("PogMacros", "UI Development Macros (Industry Grade!)", "rbxassetid://5168428496")
 
 -- widget stuff
-
 local widgetInfo = DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Right, true, true, 160, 145, 160, 145)
 local widget =  plugin:CreateDockWidgetPluginGui("PogMacros", widgetInfo)
-
 widget.Title = "Pog Macros"
 
 -- button binds
-
 button.Click:Connect(function()
 	widget.Enabled = not widget.Enabled
 	button:SetActive(widget.Enabled)
