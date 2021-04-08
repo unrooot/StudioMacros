a5f0ed0e03c33b52c0f3d5ee04639a9283a39c88
unrooot <64768843+unrooot@users.noreply.github.com>
2021-04-07 23:35:33 -0700

    whitespace gone 🦀🦀

--- src/plugin.client.lua
+++ src/plugin.client.lua
@@ -6,14 +6,14 @@ for _, module in pairs(macros:GetDescendants()) do
 	if module:IsA("ModuleScript") then
 		local data = require(module)
 		local macro = plugin:CreatePluginAction(data.name, data.name, data.desc, "rbxassetid://5972593639", true)
-        
+
         if data.init then
             spawn(function() data.init(plugin) end)
         end
-        
+
 		macro.Triggered:Connect(function()
 			local selected = Selection:Get()
-            
+
 			if #selected > 0 then
 				for _, instance in ipairs(selected) do
 					if data.extended then
